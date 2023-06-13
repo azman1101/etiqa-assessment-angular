752b949878f1199e9ca20951be54bc067ad11e23
azman <azman95ict@gmail.com>
2023-06-14 06:39:44 +0800

    update url

--- src/app/url.ts
+++ src/app/url.ts
@@ -1,3 +1,3 @@
 // export default 'https://64868926beba6297278edf86.mockapi.io/'
-// export default 'https://assessment-etiqa.herokuapp.com/'
-export default 'http://localhost:3004/'
+export default 'https://assessment-etiqa-node.herokuapp.com/'
+// export default 'http://localhost:3004/'
