2d5f8ebc8f76b4547432953c86e1c427e21698bf
azman <azman95ict@gmail.com>
2023-06-14 10:56:50 +0800

    deployment

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-102'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-102/index.html'));
+  res.sendFile(__dirname + '/dist/angular-102/index.html');
 });
 app.listen(process.env.PORT || 8080);
