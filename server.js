35893a0493892465b329a145fd601535fb4def0d
azman <azman95ict@gmail.com>
2023-06-14 10:10:29 +0800

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/etiqa-assessment-angular'));
+app.use(express.static(__dirname + '/dist/angular-102'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/etiqa-assessment-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-102/index.html'));
 });
 app.listen(process.env.PORT || 8080);
